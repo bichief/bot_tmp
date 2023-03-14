6a853e0eb73ab4a08c0f31870e215d07da7e0304
bichief <sashamarx67@gmail.com>
2023-03-14 17:58:31 +0300

    upd template

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,18 +7,4 @@ from utils.db_api.db_commands import select_client, create_client
 
 @dp.message_handler(CommandStart())
 async def bot_start_no_state(message: types.Message):
-    user_id = message.from_user.id
-    user = await select_client(user_id)
-    if not user:
-        await create_client(username=message.from_user.username, telegram_id=user_id)
-        await message.answer("\n".join(
-            [
-                f'Вы добалены в базу данных'
-            ]
-        ))
-    else:
-        await message.answer("\n".join(
-            [
-                f'Вы уже есть в базе данных'
-            ]
-        ))
+    await message.answer('Привет!')
